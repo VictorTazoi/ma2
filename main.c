9c8a91cbfc97a19002a0aa9c341c279c5fc21349
VictorTazoi <victortazoi@hotmail.com>
2025-06-06 21:29:34 -0300

    adicionado listagem

--- main.c
+++ main.c
@@ -13,24 +13,37 @@ typedef struct{
 
 int menuAcao = 0; //Variável de controle das opções do menu inicial
 
+int total = 3;
+
+//CADASTRAR
 int cadastrarAtivo(){
     system("cls");
     printf("CADASTRO DE ATIVO\n");
-    printf("\nBloco de código da função aqui");
 
     printf("\n\nPressione 0 para VOLTAR: ");
     scanf("%i", &menuAcao);
     printf("\n\n");;
 }
-void listarAtivos(){
+
+//LISTAR
+void listarAtivos(Ativo ativos[], int total){
     system("cls");
     printf("LISTAR ATIVOS\n");
-    printf("\nBloco de código da função aqui");
+
+    float lucro = 0;
+    printf("===Lista de Ativos:===\n");
+    printf("Codigo\t Valor de Compra\t\t Valor de Venda\t\t Lucro\n");
+    for (int i = 0; i< total; i++) {
+        ativos[i].lucro = ativos[i].valor_venda - ativos[i].valor_compra;
+        printf("%s \t\t %f \t\t %f \t\t %f\n",ativos[i].codigo,ativos[i].valor_compra,ativos[i].valor_venda,ativos[i].lucro);
+    }
 
     printf("\n\nPressione 0 para VOLTAR: ");
     scanf("%i", &menuAcao);
     printf("\n\n");
 }
+
+//BUSCAR
 int buscarAtivo(){
     system("cls");
     printf("BUSCAR UM ATIVO\n");
@@ -40,26 +53,28 @@ int buscarAtivo(){
     scanf("%i", &menuAcao);
     printf("\n\n");
 }
+
+//ATUALIZAR
 int atualizarAtivo(){
     system("cls");
     printf("ATUALIZAR UM ATIVO\n");
+
     printf("\nBloco de código da função aqui");
 
     printf("\n\nPressione 0 para VOLTAR: ");
     scanf("%i", &menuAcao);
     printf("\n\n");
 }
+
+//EXCLUIR
 int excluirAtivo(Ativo ativos[],int total){
     system("cls");
-
     printf("EXCLUIR UM ATIVO\n");
 
     char cod;
     int k = 0;
-
     printf("Qual o código do ativo a ser removido?\n");
     scanf("%s",&cod);
-
     for(int i = 0;i<total;i++){
         if(strcmp(ativos[i].codigo,cod) == 0){
             k++;
@@ -78,7 +93,8 @@ int main()
 {
     setlocale(LC_ALL,"");
 
-    int total = 40;
+
+    Ativo ativos[total];
 
     do{
         system("cls");
@@ -96,13 +112,13 @@ int main()
         if(menuAcao == 1){ //Cadastrar Ativo
             cadastrarAtivo();
         }else if(menuAcao == 2){ // Listar Ativo
-            listarAtivos();
+            listarAtivos(ativos, total);
         }else if(menuAcao == 3){ // Buscar Ativo
             buscarAtivo();
         }else if(menuAcao == 4){ // Atualizar Ativo
             atualizarAtivo();
         }else if(menuAcao == 5){ // Excluir Ativo
-            excluirAtivo();
+            excluirAtivo(ativos, total);
         }
 
         if((menuAcao < 0) || (menuAcao > 5)){
